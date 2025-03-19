9838d1003b6e51e55373428af317f6c73b4d50e0
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-03-19 14:21:05 +0800

    push

--- src/app/QrAttendance.tsx
+++ src/app/QrAttendance.tsx
@@ -7,23 +7,13 @@ import { useState } from 'react';
 export const QrAttendance = () => {
   const navigate = useNavigate();
 
-  const [scanResult, setScanResult] = useState('');
-  const handleScan = (data: string | null) => {
-    if (data) {
-      setScanResult(data); 
-    }
-  };
-
-  const handleError = (err: any) => {
-    console.error('Error scanning QR code: ', err); 
-  };
-
   return (
     <div className='h-screen'>
       <Button onClick={() => navigate("/login")}>Login</Button>
       <div className='h-full w-full grid grid-cols-2 gap-10 p-10 items-center justify-center'>
         <div className='w-full h-[90%] bg-gray-200'>
-         <Scanner onScan={(result) => console.log(result.map((v) => v.rawValue))} />;
+          <Scanner onScan={(result) => console.log(result.map((v) => v.rawValue))} />
+          <span>push</span>
         </div>
         <div className='w-full h-[90%] p-4'>
           <QRAttendanceTable />
